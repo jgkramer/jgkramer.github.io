eccfa3da905b67eed81340921aa16c34460d28e8
Jared Kramer <jared.kramer@gmail.com>
2022-10-13 23:20:55 -0700

    adding images to post 1

---
+++ _posts/2022-10-17_Residential_Electricity_Rates.py
@@ -0,0 +1,18 @@
+---
+title: "Introduction to Residential Electricity Rates"
+date: "2022-10-17"
+---
+
+
+
+
+
+
+Nevada Electricity Rates by Season and Time of Day.
+
+![NV Rates](../charts/post1/output_NV.png)
+
+
+Connecticut Electricity Rates by Season and Time of Day.
+
+![CT Rates](../charts/post1/output_CT.png)
